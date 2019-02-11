73aeec1d99a58fd8939d4c943242d12a2d14fb4b
Richard Kotze <rkotze@findmypast.com>
2019-02-11 18:40:42 +0000

    Project configurations added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const vscode = require('vscode');
 const { toggleDetails } = require('./commands/toggle-details');
 
 function activate(context) {
   toggleDetails({ context });
+  const projects = vscode.workspace.getConfiguration('LanceDarkly').get('projects');
 }
 
 exports.activate = activate;
