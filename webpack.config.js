c2b5dc36e712bf15b59f8e094ab1833e961b59da
Richard Kotze <rkotze@findmypast.com>
2019-04-01 18:50:01 +0100

    Use webpack to build both web & vs code js bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
-module.exports = {
+const webClient = {
+  target: "web",
   entry: ["babel-polyfill", "./src/webviews/index.js"],
   output: {
     filename: "bundle.js",
@@ -25,3 +26,36 @@ module.exports = {
     ignored: /node_modules/
   }
 };
+
+const nodeJs = {
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]"
+  },
+  devtool: "source-map",
+  externals: {
+    vscode: "vscode",
+    "node-fetch": "node-fetch"
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  watch: true,
+  watchOptions: {
+    ignored: /node_modules/
+  }
+};
+
+module.exports = [webClient, nodeJs];
