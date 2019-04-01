f290e95d193314ae1fe854ef5dafda4eeb7de9cf
Richard Kotze <rkotze@findmypast.com>
2019-04-01 17:32:48 +0100

    Prevent an infinite loop appearing when updating state.
    - useEffect is called everytime the component is called which happens when updating state
    - A guard is needed to prevent the state triggering
    - Get the previous props using a custom hook

--- src/webviews/list-plugins.js
+++ src/webviews/list-plugins.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { VsCodeContext } from "./vs-code-context/index";
 
 import { ThemeLabel, PluginLayout } from "./toggle-data-layout.styles";
+import { usePrevious } from "./use-previous";
 
 export function ListPlugins({ ldKey }) {
   const { vscodeSubscribe, vscode } = useContext(VsCodeContext);
   const [fields, setFields] = useState([]);
-  useEffect(() => {
-    vscodeSubscribe(event => {
-      const { resolvePlugins } = event.data;
-      if (resolvePlugins) {
-        setFields(resolvePlugins);
-      }
-    });
+  const prevLdKey = usePrevious(ldKey);
 
+  vscodeSubscribe(event => {
+    const { resolvePlugins } = event.data;
+    if (resolvePlugins) {
+      setFields(resolvePlugins);
+    }
+  });
+
+  if (ldKey !== prevLdKey) {
     vscode.postMessage({
       name: "resolvePlugins",
       args: [ldKey, "detailFields"]
     });
-  });
+  }
 
   if (fields.length) {
     try {
@@ -29,7 +32,7 @@ export function ListPlugins({ ldKey }) {
               <h3>{title}</h3>
               {fields.map(list => (
                 <PluginLayout>
-                  {list.slice(0,4).map((field, i) => {
+                  {list.slice(0, 4).map((field, i) => {
                     if (i === 0) return <ThemeLabel>{field}</ThemeLabel>;
                     return <span>{field}</span>;
                   })}
