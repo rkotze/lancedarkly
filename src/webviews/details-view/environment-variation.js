d1a923c1ddb01e2ed200143b2da9b9368d317752
Richard Kotze <rkotze@findmypast.com>
2019-04-08 17:41:53 +0100

    Show selected toggle value for on or off state

--- src/webviews/details-view/environment-variation.js
+++ src/webviews/details-view/environment-variation.js
@@ -1,17 +1,45 @@
 import React from "react";
 import { LightBadge } from "../badge.styles";
 
-export function EnvironmentVariation({ variations, envDetails }) {
+function VariationView({ variation, variationList }) {
+  if (variationList) {
+    return (
+      <div>
+        {variationList.map(({ variation, weight }) => {
+          return (
+            <span>
+              {weight}% <LightBadge>{variation}</LightBadge>
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
   return (
     <div>
-      {!envDetails.fallthrough.rollout && (
-        <span>
-          Default: 100%{" "}
-          <LightBadge>
-            {String(variations[envDetails.fallthrough.variation].value)}
-          </LightBadge>
-        </span>
-      )}
+      <LightBadge>{variation}</LightBadge>
     </div>
   );
+}
+
+export function EnvironmentVariation({ variations, envDetails }) {
+  const { fallthrough, offVariation, on } = envDetails;
+  if (on && !fallthrough.rollout) {
+    return (
+      <VariationView
+        variation={String(variations[fallthrough.variation].value)}
+      />
+    );
+  }
+  if (on && fallthrough.rollout) {
+    const variationList = fallthrough.rollout.variations.map(toggle => {
+      return {
+        variation: String(variations[toggle.variation].value),
+        weight: toggle.weight / 1000
+      };
+    });
+    return <VariationView variationList={variationList} />;
+  }
+
+  return <VariationView variation={String(variations[offVariation].value)} />;
 }
--- src/webviews/details-view/environment-variation.spec.js
+++ src/webviews/details-view/environment-variation.spec.js
@@ -4,27 +4,6 @@ import { shallow } from "enzyme";
 import { EnvironmentVariation } from "./environment-variation";
 
 describe("Environment variation", function() {
-  const envDetailsRollout = {
-    on: false,
-    archived: false,
-    rules: [],
-    fallthrough: {
-      rollout: {
-        variations: [
-          {
-            variation: 0,
-            weight: 50000
-          },
-          {
-            variation: 1,
-            weight: 50000
-          }
-        ]
-      }
-    },
-    offVariation: 0,
-    _environmentName: "Production"
-  };
   const variations = [
     {
       value: false,
@@ -42,7 +21,7 @@ describe("Environment variation", function() {
       archived: false,
       rules: [],
       fallthrough: {
-        variation: 0
+        variation: 1
       },
       offVariation: 0,
       _environmentName: "Production"
@@ -51,6 +30,65 @@ describe("Environment variation", function() {
       <EnvironmentVariation variations={variations} envDetails={envDetails} />
     );
 
-    expect(render.text()).toContain("false");
+    expect(render.find("VariationView").prop("variation")).toEqual("true");
+  });
+
+  it("when toggle is off is shows the off variation", function() {
+    const envDetails = {
+      on: false,
+      archived: false,
+      rules: [],
+      fallthrough: {
+        variation: 1
+      },
+      offVariation: 0,
+      _environmentName: "Production"
+    };
+    const render = shallow(
+      <EnvironmentVariation variations={variations} envDetails={envDetails} />
+    );
+
+    expect(render.find("VariationView").prop("variation")).toEqual("false");
+  });
+
+  it("when toggle is on and rollout is set, display rollout values", function() {
+    const envDetailsRollout = {
+      on: true,
+      archived: false,
+      rules: [],
+      fallthrough: {
+        rollout: {
+          variations: [
+            {
+              variation: 0,
+              weight: 50000
+            },
+            {
+              variation: 1,
+              weight: 50000
+            }
+          ]
+        }
+      },
+      offVariation: 0,
+      _environmentName: "Production"
+    };
+    const render = shallow(
+      <EnvironmentVariation
+        variations={variations}
+        envDetails={envDetailsRollout}
+      />
+    );
+
+    expect(render.find("VariationView").prop("variationList")).toEqual([
+      {
+        variation: "false",
+        weight: 50
+      },
+      {
+        variation: "true",
+        weight: 50
+      }
+    ]);
   });
 });
