efd89634d8bb08e865661ba22075b61f0d946ac2
Richard Kotze <rkotze@findmypast.com>
2019-04-09 18:07:41 +0100

    WIP: make render prop to handle toggle state with variation data.

---
+++ src/webviews/details-view/handle-toggle-state.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { VsCodeContext } from "../vs-code-context/index";
+
+import { SwitchBadge } from "../badge.styles";
+
+export class HandleToggleState extends Component {
+  constructor(props) {
+    super(props);
+    const { envDetails } = props;
+    this.env = envDetails._environmentName.toLowerCase();
+    this.state = {
+      on: envDetails.on
+    };
+  }
+
+  handleToggleState = () => {
+    const { vscode } = this.context;
+    const { ldKey } = this.props;
+    const { on } = this.state;
+    vscode.postMessage({
+      name: "confirmToggleState",
+      args: [ldKey, this.env, !on]
+    });
+  };
+
+  componentDidMount() {
+    const { vscodeSubscribe } = this.context;
+    const { envDetails, ldKey } = this.props;
+    vscodeSubscribe(event => {
+      const { confirmToggleState } = event.data;
+      if (
+        confirmToggleState &&
+        this.env === confirmToggleState.env &&
+        ldKey === confirmToggleState.key
+      ) {
+        this.setState({
+          on: confirmToggleState.on
+        });
+        envDetails.on = confirmToggleState.on;
+      }
+    });
+  }
+
+  render() {
+    const { on } = this.state;
+    const { children, envDetails } = this.props;
+    return (
+      <React.Fragment>
+        {children(handleToggleState, { envDetails })}
+      </React.Fragment>
+    );
+  }
+}
+
+HandleToggleState.contextType = VsCodeContext;
--- src/webviews/details-view/toggle-badge.js
+++ src/webviews/details-view/toggle-badge.js
@@ -1,55 +1,66 @@
-import React, { Component } from "react";
-import { VsCodeContext } from "../vs-code-context/index";
+import React from "react";
+// import { VsCodeContext } from "../vs-code-context/index";
 
 import { SwitchBadge } from "../badge.styles";
 
-export class ToggleBadge extends Component {
-  constructor(props) {
-    super(props);
-    const { envDetails } = props;
-    this.env = envDetails._environmentName.toLowerCase();
-    this.state = {
-      on: envDetails.on
-    };
-  }
+// export class ToggleBadge extends Component {
+//   constructor(props) {
+//     super(props);
+//     const { envDetails } = props;
+//     this.env = envDetails._environmentName.toLowerCase();
+//     this.state = {
+//       on: envDetails.on
+//     };
+//   }
 
-  handleToggleState = () => {
-    const { vscode } = this.context;
-    const { ldKey } = this.props;
-    const { on } = this.state;
-    vscode.postMessage({
-      name: "confirmToggleState",
-      args: [ldKey, this.env, !on]
-    });
-  };
+//   handleToggleState = () => {
+//     const { vscode } = this.context;
+//     const { ldKey } = this.props;
+//     const { on } = this.state;
+//     vscode.postMessage({
+//       name: "confirmToggleState",
+//       args: [ldKey, this.env, !on]
+//     });
+//   };
 
-  componentDidMount() {
-    const { vscodeSubscribe } = this.context;
-    const { envDetails, ldKey } = this.props;
-    vscodeSubscribe(event => {
-      const { confirmToggleState } = event.data;
-      if (
-        confirmToggleState &&
-        this.env === confirmToggleState.env &&
-        ldKey === confirmToggleState.key
-      ) {
-        this.setState({
-          on: confirmToggleState.on
-        });
-        envDetails.on = confirmToggleState.on;
-      }
-    });
-  }
-  render() {
-    const { on } = this.state;
-    return (
-      <div>
-        <SwitchBadge toggleState={on} onClick={this.handleToggleState}>
-          {on ? "On" : "Off"}
-        </SwitchBadge>{" "}
-      </div>
-    );
-  }
+//   componentDidMount() {
+//     const { vscodeSubscribe } = this.context;
+//     const { envDetails, ldKey } = this.props;
+//     vscodeSubscribe(event => {
+//       const { confirmToggleState } = event.data;
+//       if (
+//         confirmToggleState &&
+//         this.env === confirmToggleState.env &&
+//         ldKey === confirmToggleState.key
+//       ) {
+//         this.setState({
+//           on: confirmToggleState.on
+//         });
+//         envDetails.on = confirmToggleState.on;
+//       }
+//     });
+//   }
+//   render() {
+// const { on } = this.state;
+// return (
+//   <div>
+//     <SwitchBadge toggleState={on} onClick={this.handleToggleState}>
+//       {on ? "On" : "Off"}
+//     </SwitchBadge>{" "}
+//   </div>
+// );
+//   }
+// }
+
+// ToggleBadge.contextType = VsCodeContext;
+
+export function ToggleBadge({ envDetails, onClick }) {
+  const { on } = envDetails;
+  return (
+    <div>
+      <SwitchBadge toggleState={on} onClick={onClick}>
+        {on ? "On" : "Off"}
+      </SwitchBadge>{" "}
+    </div>
+  );
 }
-
-ToggleBadge.contextType = VsCodeContext;
